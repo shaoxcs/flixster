7b622d7ac831d3337a6635d75174be2c2e9477f2
shaoxcs <shao.xue@outlook.com>
2020-09-25 22:28:57 -0700

    Finish the landscape view

--- app/src/main/java/com/codepath/shao/flixter/models/Movie.java
+++ app/src/main/java/com/codepath/shao/flixter/models/Movie.java
@@ -14,8 +14,10 @@ public class Movie {
     String posterPath;
     String title;
     String overview;
+    String backdropPath;
 
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
@@ -27,6 +29,10 @@ public class Movie {
             movies.add(new Movie(movieJsonArray.getJSONObject(i)));
         }
         return movies;
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
 
     public String getPosterPath() {
